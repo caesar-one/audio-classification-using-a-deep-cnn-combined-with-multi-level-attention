d1ace3c7869af9d4624a84a502c14ff3dbf8c666
caesar-one <cesare.campagnano@gmail.com>
2020-03-08 16:44:28 +0100

    Dataset load and preprocessing

--- dataset.py
+++ dataset.py
@@ -1,24 +1,52 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import librosa
 import librosa.display
-import torch
-from torch import nn
-import torch
 from glob import glob
 from tqdm import tqdm
+import pickle
 
 dataset_path = "UrbanSound8K/audio"
 metadata_path = "UrbanSound8K/metadata/UrbanSound8K.csv"
+samples_number = 88200
+
+def load():
+    if len(glob("*.pkl")):
+        with open("audio_X_train.pkl", "rb") as f: X_train = pickle.load(f)
+        with open("audio_y_train.pkl", "rb") as f: y_train = pickle.load(f)
+        with open("audio_X_test.pkl", "rb") as f: X_test = pickle.load(f)
+        with open("audio_y_test.pkl", "rb") as f: y_test = pickle.load(f)
+    else:
+        wav_paths = glob(dataset_path + "/**/*.wav", recursive=True)
+        wav_paths_train, wav_paths_test = [], []
+        for p in wav_paths:
+            if p.split("/")[-2] in ["fold1","fold2"]: wav_paths_test.append(p)
+            else: wav_paths_train.append(p)
+        metadata = pd.read_csv(metadata_path)
+        name2class = dict(zip(metadata["slice_file_name"],metadata["class"]))
+        X_train, y_train, X_test, y_test = [], [], [], []
+        for paths,setname in zip([wav_paths_train, wav_paths_test],["train","test"]):
+            for w in tqdm(paths, desc = "Converting songs in spectrograms"):
+                song, sr = librosa.load(w)
+                song = song[:samples_number]
+                reshaped_song = np.zeros((samples_number,))
+                reshaped_song[:song.shape[0]]=song
+                spectrogram = librosa.feature.melspectrogram(reshaped_song)
+                song_filename = w.split("/")[-1]
+                if setname == "train":
+                    X_train.append(spectrogram)
+                    y_train.append(name2class[song_filename])
+                else:
+                    X_test.append(spectrogram)
+                    y_test.append(name2class[song_filename])
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
+        with open("audio_X_train.pkl","wb") as f: pickle.dump(X_train,f)
+        with open("audio_y_train.pkl","wb") as f: pickle.dump(y_train,f)
+        with open("audio_X_test.pkl","wb") as f: pickle.dump(X_test,f)
+        with open("audio_y_test.pkl","wb") as f: pickle.dump(y_test,f)
+    return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
-    wav_paths = glob(dataset_path + "/**/*.wav", recursive=True)
-    metadata = pd.read_csv(metadata_path)
-    name2class = dict(zip(metadata["slice_file_name"],metadata["class"]))
-    X, y = [], []
-    for w in tqdm(wav_paths):
-        song, sr = librosa.load(w)
-        X.append(song)
-        song_filename = w.split("/")[-1]
-        y.append(name2class[song_filename])
+    X_train, X_test, y_train, y_test = load()
