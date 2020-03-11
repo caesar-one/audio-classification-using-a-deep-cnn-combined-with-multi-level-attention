816f22468ce34f6cd235f3e57386a7875766ccfd
caesar-one <cesare.campagnano@gmail.com>
2020-03-11 15:27:17 +0100

    added options to fine-tune or extract features from the CNN model

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
+import torch
 import numpy as np
 from torch import nn
 import torch.nn.functional as F
+from torchvision.models import resnet50
 
 # TODO Different channels? can put derivative? see video
 
@@ -10,6 +12,47 @@ H = 600   # size of hidden layers, TODO 500
 DR = 0.4  # dropout rate, TODO 0.2
 L = 6     # number of embedded mappings
 K = 10    # number of classes
+
+
+def set_requires_grad(model, value):
+    for param in model.parameters():
+        param.requires_grad = value
+
+
+def initialize_cnn(num_classes, # number of output classes (makes sense only if combined with just_bottleneck=False)
+                   use_pretrained=True, # Uses a pretrained version of resnet50
+                   just_bottleneck=False, # if =True the FC part is removed, so that the model returns bottlenecks.
+                   cnn_trainable=False, # if =True CNN part is trainable. Otherwise the gradient will NOT be calculated
+                   first_cnn_layer_trainable=False, # Sets the first CNN layer trainable, to optimize for the dataset
+                   in_channels=3): # the the number of input channels is reshaped
+
+    m = resnet50(pretrained=use_pretrained)
+    input_size = 224
+
+    if not cnn_trainable:
+        set_requires_grad(m, False)
+
+    if first_cnn_layer_trainable:
+        if in_channels == 3:
+            set_requires_grad(m.conv1, True)
+        else:
+            m.conv1=nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
+
+    if just_bottleneck:
+        modules = list(m.children())[:-1]  # delete the last fc layer.
+        modules.append(CnnFlatten()) # TODO Rivedere questa istruzione (e solo questa!)
+        m = nn.Sequential(*modules)
+
+    else:
+        num_ftrs = m.fc.in_features
+        m.fc = nn.Linear(num_ftrs, num_classes)
+
+    return m, input_size
+
+
+class CnnFlatten(nn.Module):
+    def forward(self, input):
+        return torch.flatten(input, 1) #TODO check second parameter (it should be correct considering batches)
 
 
 # Implements the block g (green big block in the main paper)
