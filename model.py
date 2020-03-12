29350d84d4459549d6c5997749433a1316bb02f1
Lorenzo Di Marzio <1lorenzo.dimarzio@gmail.com>
2020-03-12 21:03:39 +0100

    attention fixed

--- model.py
+++ model.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 
 # TODO Different channels? can put derivative? see video
 
-T = 4     # number of bottleneck features, TODO it depends on audio clip length? (paper2)
-M = 128   # size of a bottleneck feature
-H = 600   # size of hidden layers, TODO 500
+T = 4  # number of bottleneck features, TODO it depends on audio clip length? (paper2)
+M = 128  # size of a bottleneck feature
+H = 600  # size of hidden layers, TODO 500
 DR = 0.4  # dropout rate, TODO 0.2
-L = 6     # number of levels
-K = 10    # number of classes
+L = 6  # number of levels
+K = 10  # number of classes
 
 
 def set_requires_grad(model, value):
@@ -20,12 +20,12 @@ def set_requires_grad(model, value):
         param.requires_grad = value
 
 
-def initialize_cnn(num_classes, # number of output classes (makes sense only if combined with just_bottleneck=False)
-                   use_pretrained=True, # Uses a pretrained version of resnet50
-                   just_bottleneck=False, # if =True the FC part is removed, so that the model returns bottlenecks.
-                   cnn_trainable=False, # if =True CNN part is trainable. Otherwise the gradient will NOT be calculated
-                   first_cnn_layer_trainable=False, # Sets the first CNN layer trainable, to optimize for the dataset
-                   in_channels=3): # the the number of input channels is reshaped
+def initialize_cnn(num_classes,  # number of output classes (makes sense only if combined with just_bottleneck=False)
+                   use_pretrained=True,  # Uses a pretrained version of resnet50
+                   just_bottleneck=False,  # if =True the FC part is removed, so that the model returns bottlenecks.
+                   cnn_trainable=False,  # if =True CNN part is trainable. Otherwise the gradient will NOT be calculated
+                   first_cnn_layer_trainable=False,  # Sets the first CNN layer trainable, to optimize for the dataset
+                   in_channels=3):  # the the number of input channels is reshaped
 
     m = resnet50(pretrained=use_pretrained)
     input_size = 224
@@ -37,11 +37,12 @@ def initialize_cnn(num_classes, # number of output classes (makes sense only if 
         if in_channels == 3:
             set_requires_grad(m.conv1, True)
         else:
-            m.conv1=nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
+            m.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, stride=2, padding=3,
+                                bias=False)
 
     if just_bottleneck:
         modules = list(m.children())[:-1]  # delete the last fc layer.
-        modules.append(CnnFlatten()) # TODO Rivedere questa istruzione (e solo questa!)
+        modules.append(CnnFlatten())  # TODO Rivedere questa istruzione (e solo questa!)
         m = nn.Sequential(*modules)
 
     else:
@@ -53,31 +54,10 @@ def initialize_cnn(num_classes, # number of output classes (makes sense only if 
 
 class CnnFlatten(nn.Module):
     def forward(self, input):
-        return torch.flatten(input, 1) #TODO check second parameter (it should be correct considering batches)
+        return torch.flatten(input, 1)  # TODO check second parameter (it should be correct considering batches)
 
 
 # Implements the block g (green big block in the main paper)
-class EmbeddedMappingCat(nn.Module): # ---------> 🐈
-
-    def __init__(self):
-        super(EmbeddedMappingCat, self).__init__()
-        self.fc1 = [nn.Linear(M, H) for _ in range(T)]
-        self.fc2 = [nn.Linear(H, H) for _ in range(T)]
-        self.fc3 = [nn.Linear(H, H) for _ in range(T)]
-
-    # Input x has size (T, M)
-    def forward(self, x):
-        h1 = torch.tensor(()).new_zeros((T, H))
-        h2 = torch.tensor(()).new_zeros((T, H))
-        h = torch.tensor(()).new_zeros((T, H))
-        for t in range(T):
-            h1[t] = F.dropout(F.relu(self.fc1[t](x[t])), DR)
-            h2[t] = F.dropout(F.relu(self.fc2[t](h1[t])), DR)
-            h[t] = F.dropout(F.relu(self.fc3[t](h2[t])), DR)   # TODO forse l'ultimo dropout non ci deve essere
-        # Output h has size (T, H)
-        return h
-
-
 class EmbeddedMapping(nn.Module):
 
     def __init__(self):
@@ -86,12 +66,13 @@ class EmbeddedMapping(nn.Module):
         self.fc2 = nn.Linear(H, H)
         self.fc3 = nn.Linear(H, H)
 
-    # Input x has size M
+    # Input x has shape (batch_size, T, M)
     def forward(self, x):
-        h1 = F.dropout(F.relu(self.fc1(x)), DR)
-        h2 = F.dropout(F.relu(self.fc2(h1)), DR)
-        h = F.dropout(F.relu(self.fc3(h2)), DR)   # TODO forse l'ultimo dropout non ci deve essere
-        # Output h has size H
+        h1 = F.dropout(F.relu(self.fc1(x)), p=DR)
+        h2 = F.dropout(F.relu(self.fc2(h1)), p=DR)
+        h = F.dropout(F.relu(self.fc3(h2)), p=DR)  # TODO forse l'ultimo dropout non ci deve essere
+        # Output h has shape (batch_size, T, H)
+        print(h.shape)
         return h
 
 
@@ -103,13 +84,13 @@ class AttentionModule(nn.Module):
         self.fcv = nn.Linear(H, K)
         self.fcf = nn.Linear(H, K)
 
-    # Input h has size (T, H)
+    # Input h has shape (batch_size, T, H)
     def forward(self, h):
-        v = F.softmax(self.fcv(h))  # attention
-        f = F.sigmoid(self.fcv(h))  # classification
-        p = v / np.sum(v, 0)  # normalized attention
-        y = np.sum(f * p, 0)
-        # Output y has size (K)
+        att = F.softmax(self.fcv(h), dim=2)  # attention, shape (batch_size, T, K)
+        cla = torch.sigmoid(self.fcv(h))  # classification, shape (batch_size, T, K)
+        norm_att = att / torch.sum(att, dim=1)[:, None, :]  # normalized attention, shape (batch_size, T, K)
+        y = torch.sum(cla * norm_att, dim=1)
+        # Output y has size (batch_size, K)
         return y
 
 
@@ -127,7 +108,7 @@ class MultiLevelAttention(nn.Module):
 
 if __name__ == '__main__':
     np.random.seed(0)
-    Xtrain = torch.from_numpy(np.random.random((3, T, H))).float()
+    Xtrain = torch.from_numpy(np.random.random((32, T, H))).float()
 
     use_cuda = torch.cuda.is_available()
     print("CUDA available: " + str(use_cuda))
@@ -138,6 +119,4 @@ if __name__ == '__main__':
     net.train()
     with torch.no_grad():
         for epoch in tqdm(range(9)):
-            for Xb in Xtrain:
-                v, f, p, y = net(Xb)
-    print(y)
+            y = net(Xtrain)
