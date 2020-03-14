6b11beea00d49118d48b015037b428a41d85562d
caesar-one <cesare.campagnano@gmail.com>
2020-03-14 19:24:23 +0100

    Model fixed, dataset enhanced and added train.py module to enable model training

--- dataset.py
+++ dataset.py
@@ -34,7 +34,7 @@ def load(save = True, load_saved = True, slots_num=4, use_sliding=None):
             if p.split("/")[-2] in ["fold1","fold2"]: wav_paths_test.append(p)
             else: wav_paths_train.append(p)
         metadata = pd.read_csv(metadata_path)
-        name2class = dict(zip(metadata["slice_file_name"],metadata["class"]))
+        name2class = dict(zip(metadata["slice_file_name"],metadata["classID"]))
         X_train, y_train, X_test, y_test = [], [], [], []
         for paths,setname in zip([wav_paths_train, wav_paths_test],["train","test"]):
             for w in tqdm(paths, desc = f"Converting {setname} samples in spectrograms"):
@@ -53,10 +53,10 @@ def load(save = True, load_saved = True, slots_num=4, use_sliding=None):
                 sample_filename = w.split("/")[-1]
                 if setname == "train":
                     X_train.append(spectrograms)
-                    y_train.append(name2class[sample_filename])
+                    y_train.append(int(name2class[sample_filename]))
                 else:
                     X_test.append(spectrograms)
-                    y_test.append(name2class[sample_filename])
+                    y_test.append(int(name2class[sample_filename]))
         X_train = np.array(X_train)
         y_train = np.array(y_train)
         X_test = np.array(X_test)
