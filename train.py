5f3e742f51e47a719e5730f98806103df849281e
Lorenzo Di Marzio <1lorenzo.dimarzio@gmail.com>
2020-03-18 17:46:01 +0100

    added BatchNorm1D after each Linear

--- train.py
+++ train.py
@@ -95,13 +95,12 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
-            print(model_ft.mla.embedded_mappings[0].weight.grad)
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            print(epoch_loss, running_loss, len(dataloaders[phase].dataset))
+            #print(epoch_loss, running_loss, len(dataloaders[phase].dataset))
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
-            print('{} Loss: {:.4f}, Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            #print('{} Loss: {:.4f}, Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
