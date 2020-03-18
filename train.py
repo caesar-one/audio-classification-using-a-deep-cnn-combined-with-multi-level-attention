01214f07b0c11e8ea667fdea1ada6e02b84901e5
Lorenzo Di Marzio <1lorenzo.dimarzio@gmail.com>
2020-03-18 18:03:56 +0100

    added BatchNorm1D after each Linear

--- train.py
+++ train.py
@@ -94,6 +94,8 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
+            print('{} Loss: {:.4f}, Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
