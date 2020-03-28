895cf7c07a022585e63440b5294e228309790df8
caesar-one <cesare.campagnano@gmail.com>
2020-03-28 16:55:27 +0100

    Dataset class added (to cycle on the HDF5 file remotely)

--- params.py
+++ params.py
@@ -24,7 +24,7 @@ TARGET_NAMES = ['air_conditioner', 'car_horn', 'children_playing', 'dog_bark', '
 S_RESNET_SHAPE = (224, 224)
 S_VGGISH_SHAPE = (96, 64)
 
-T = 4  # number of bottleneck features
+T = 10  # number of bottleneck features
 M_RESNET = 2048  # size of a bottleneck feature (resnet)
 M_VGGISH = 128 # size of a bottleneck feature (vggish)
 M_VGGISH_JB = 512 * 6 * 4
